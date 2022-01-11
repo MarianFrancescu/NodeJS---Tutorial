e47b34c6f9009608f40293127cf3740c637fe2b0
Marian Francescu <marian.francescu98@gmail.com>
2022-01-11 14:18:51 +0200

    Add auth dependency in router

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,11 @@
 const controller = require("./../controllers/controller");
+const authUser = require('./../controllers/auth');
 
 module.exports = function(router) {
     router.get('/', controller.getDefault);
     router.post('/addWeight', controller.addWeight);
     router.get('/aboutus', controller.aboutUs);
-    router.get('/getEmployees', controller.getEmployees);
+    router.get('/getEmployees', authUser, controller.getEmployees);
     router.delete('/deleteByName', controller.deleteByName);
     router.post('/addEmployee', controller.addEmployee);
     router.put('/updateEmployee', controller.updateEmployee);
